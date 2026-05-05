5baa1df597dd3f4a4733f985fe6cd14e5e5e4b01
Harry Cordewener <admin@twilightdays.org>
2026-05-04 22:28:28 -0500

    Update logo viewBox and script to match

--- scripts/generate_logo.py
+++ scripts/generate_logo.py
@@ -18,7 +18,7 @@ def generate_svg():
         font_b64 = base64.b64encode(f.read()).decode()
 
     svg = f'''<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="435 88 352 108">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="435 88 371 126">
   <defs>
     <style>
       @font-face {{
